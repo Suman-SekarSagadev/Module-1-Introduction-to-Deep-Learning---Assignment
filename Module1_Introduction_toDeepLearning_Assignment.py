6638067780bea9c79027fa68284cff43d1b168af
Suman Sekar <139876640+Suman-SekarSagadev@users.noreply.github.com>
2024-04-07 10:28:16 +0530

    Add files via upload

---
+++ Module1_Introduction_toDeepLearning_Assignment.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Module 1: Introduction to Deep Learning - Assignment
+
+# In[2]:
+
+
+get_ipython().run_line_magic('pinfo', 'applications')
+a. Healthcare
+b. Manufacturing
+c. Retail
+d. All of the above
+
+
+Correct answer : D
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'Learning')
+a. Depth of Neural Networks
+b. Type of Algorithms used
+c. Machine Learning is an evolution of Deep Learning
+d. Volume of data processed
+i. a,d
+ii. a,b,c,d
+iii. a,c,d
+iv. a,b
+
+
+Correct Answer : I. A,D
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+3. Which metrics are commonly used to evaluate the performance of a classification
+get_ipython().run_line_magic('pinfo', 'model')
+a. Mean squared error
+b. Root mean squared error
+c. F1 score
+d. R-squared
+
+
+Correct Answer : C . F1 Score
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+4. Which of the following are the key components of Deep Learning (DL)?
+a. Decision Trees
+b. Neural Networks
+c. Support Vector Machines
+d. K-Means Clustering
+
+Correct answer : b. Neural Networks
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'networks')
+a. Lack of labeled data
+b. Overfitting
+c. Underfitting
+d. None of the above
+
+Correct Answer : d. None of the above
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'data')
+a. Artificial Intelligence 
+b. Machine Learning 
+c.Deep Learning
+
+Correct Answer : b. Machine Learning 
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'network')
+a. A network of interconnected computers
+b. A network of interconnected neurons
+c. A network of interconnected databases
+d. A network of interconnected algorithms
+
+
+Correct Answer : b. A network of interconnected neurons
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'phase')
+a. To adjust the learning rate
+b. To optimize model parameters for better performance
+c. To select features
+d. To preprocess the data
+
+
+Correct ANswer : a. To adjust the learning rate
+
+
+# In[ ]:
+
+
+9. What is the primary goal during the project initiation phase of a Deep Learning
+get_ipython().run_line_magic('pinfo', 'project')
+a. Model training
+b. Define the problem and scope
+c. Hyperparameter tuning
+d. Data collection
+
+
+Correct Answer : b. Define the problem and scope
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+10. Why is continuous monitoring important in the life cycle of a deployed Deep
+get_ipython().run_line_magic('pinfo', 'model')
+a. To retrain the model from scratch
+b. To ensure the model adapts to changing data patterns
+c. To deploy a new model version daily
+d. To skip t
+
+Correct Answer : b. To ensure the model adapts to changing data patterns
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
